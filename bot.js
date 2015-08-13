a86ee511e83f0c31c5fa55b65aed015792a6f60d
Rapptz <rapptz@gmail.com>
2015-08-13 19:16:55 -0400

    Switch to discord.js. Add !cleanup, !info, and !authority.

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-var Discord = require('node-discord');
+var Discord = require('discord.js');
 var request = require('request');
 var fs = require('fs');
 
@@ -8,15 +8,14 @@ function load_config() {
     config = JSON.parse(fs.readFileSync('config.json').toString());
 }
 
+function save_config() {
+    fs.writeFileSync('config.json', JSON.stringify(config, null, 4), 'utf-8');
+}
 
 load_config();
 
-var bot = new Discord({
-    username: config.username,
-    password: config.password,
-    chats: config.chats,
-    autorun: true
-});
+var bot = new Discord.Client();
+bot.login(config.username, config.password);
 
 var commands = {};
 
@@ -28,32 +27,15 @@ var authority_prettify = {
 };
 
 
-/* begin polyfill */
-
-if (!Array.prototype.find) {
-  Array.prototype.find = function(predicate) {
-    if (this === null) {
-      throw new TypeError('Array.prototype.find called on null or undefined');
+function find_from(list, predicate) {
+    for(var i = 0; i < list.length; ++i) {
+        var value = list[i];
+        if(predicate(value)) {
+            return value;
+        }
     }
-    if (typeof predicate !== 'function') {
-      throw new TypeError('predicate must be a function');
-    }
-    var list = Object(this);
-    var length = list.length >>> 0;
-    var thisArg = arguments[1];
-    var value;
-
-    for (var i = 0; i < length; i++) {
-      value = list[i];
-      if (predicate.call(thisArg, value, i, list)) {
-        return value;
-      }
-    }
-    return undefined;
-  };
+    return null;
 }
-
-/* end polyfill */
 
 function get_user_authority(userID) {
     return config.authority[userID] || 0;
@@ -89,7 +71,7 @@ function command_is_hidden(key) {
 
 commands.help = {
     help: 'shows this message',
-    command: function(opts) {
+    command: function(message) {
         var text = 'available commands:\n';
         for(var key in commands) {
             if(command_is_hidden(key)) {
@@ -102,55 +84,55 @@ commands.help = {
                 text = text.concat(config.command_prefix, key, '\n');
             }
         }
-        bot.sendMessage({ to: opts.chatID, message: text });
+        bot.sendMessage(message.channel, text);
     }
 };
 
 commands.hello = {
     help: 'displays my intro message!',
-    command: function(opts) {
-        bot.sendMessage({ to: opts.chatID, message: "Hello! I'm a robot! Danny made me."  });
+    command: function(message) {
+        bot.sendMessage(message.channel, "Hello! I'm a robot! Danny made me.");
     }
 };
 
 commands.random = {
     help: 'displays a random weapon, map, or number',
-    command: function(opts) {
+    command: function(message) {
         var error_string = 'Random what? weapon, map, or number? (e.g. !random weapon)'
-        if(opts.words.length < 1) {
-            bot.sendMessage({ to: opts.chatID, message: error_string });
+        if(message.args.length < 1) {
+            bot.sendMessage(message.channel, error_string);
             return;
         }
 
-        var type = opts.words[0].toLowerCase();
+        var type = message.args[0].toLowerCase();
         if(type == 'weapon') {
             var index = Math.floor(Math.random() * config.splatoon.weapons.length);
-            bot.sendMessage({ to: opts.chatID, message: config.splatoon.weapons[index] });
+            bot.sendMessage(message.channel, config.splatoon.weapons[index]);
         }
         else if(type == 'number') {
-            bot.sendMessage({ to: opts.chatID, message: Math.floor(Math.random() * 100).toString() });
+            bot.sendMessage(message.channel, Math.floor(Math.random() * 100).toString());
         }
         else if(type == 'map') {
             var index = Math.floor(Math.random() * config.splatoon.maps.length);
-            bot.sendMessage({ to: opts.chatID, message: config.splatoon.maps[index] });
+            bot.sendMessage(message.channel, config.splatoon.maps[index]);
         }
         else {
-            bot.sendMessage({ to: opts.chatID, message: error_string });
+            bot.sendMessage(message.channel, error_string);
         }
     }
 };
 
-function get_splatoon_map_callback(index, prefix) {
+function get_splatoon_map_callback(index, prefix, current_channel) {
     return function(error, response, body) {
         if(error || response.statusCode != 200) {
-            bot.sendMessage({ to: opts.chatID, message: "Unfortunately an error occurred. Tell Danny the error was " + error });
+            bot.sendMessage(current_channel, "Unfortunately an error occurred. Tell Danny the error was " + error);
             return;
         }
 
         var data = JSON.parse(body);
         var schedule = data['schedule'];
         if(!schedule || schedule.length == 0) {
-            bot.sendMessage({ to: opts.chatID, message: "Maps could not be found..." });
+            bot.sendMessage(current_channel, "Maps could not be found...");
             return;
         }
 
@@ -158,7 +140,7 @@ function get_splatoon_map_callback(index, prefix) {
         var result = '';
         result = result.concat(prefix, ' regular maps: ', current_maps.regular.maps[0].nameEN, ' and ', current_maps.regular.maps[1].nameEN, '\n');
         result = result.concat(prefix, ' ', current_maps.ranked.rulesEN, ' maps: ', current_maps.ranked.maps[0].nameEN, ' and ', current_maps.ranked.maps[1].nameEN);
-        bot.sendMessage({ to: opts.chatID, message: result });
+        bot.sendMessage(current_channel, result);
     };
 }
 
@@ -168,35 +150,21 @@ function to_percent(value) {
 
 commands.maps = {
     help: 'shows the current Splatoon maps in rotation',
-    command: function(opts) {
-        request(config.splatoon.schedule_url, get_splatoon_map_callback(0, 'Current'));
+    command: function(message) {
+        request(config.splatoon.schedule_url, get_splatoon_map_callback(0, 'Current', message.channel));
     }
 };
 
 commands.nextmaps = {
     help: 'shows the next Splatoon maps in rotation',
-    command: function(opts) {
-        request(config.splatoon.schedule_url, get_splatoon_map_callback(1, 'Next'));
-    }
-};
-
-commands.index = {
-    authority: 3,
-    command: function(opts) {
-        bot.sendMessage({ to: opts.chatID, message: 'Chat ID: ' + opts.chatID + ' userID: ' + opts.userID + ' username: ' + opts.user });
-    }
-};
-
-commands.raw = {
-    authority: 3,
-    command: function(opts) {
-        bot.sendMessage({ to: opts.chatID, message: JSON.stringify(opts.rawEvent) });
+    command: function(message) {
+        request(config.splatoon.schedule_url, get_splatoon_map_callback(1, 'Next', message.channel));
     }
 };
 
 commands.quit = {
     authority: 3,
-    command: function(opts) {
+    command: function(message) {
         bot.disconnect();
         process.exit(0);
     }
@@ -204,84 +172,145 @@ commands.quit = {
 
 commands.choose = {
     help: 'helps choose between multiple choices',
-    command: function(opts) {
-        if(opts.words.length < 2) {
-            bot.sendMessage({ to: opts.chatID, message: 'Not enough choices to choose from... (e.g. !choose 1 2 3)' });
+    command: function(message) {
+        if(message.args.length < 2) {
+            bot.sendMessage(message.channel, 'Not enough choices to choose from... (e.g. !choose 1 2 3)');
             return;
         }
 
-        var random_index = Math.floor(Math.random() * opts.words.length);
-        bot.sendMessage({ to: opts.chatID, message: opts.words[random_index] });
+        var random_index = Math.floor(Math.random() * message.args.length);
+        bot.sendMessage(message.channel, message.args[random_index]);
     }
 };
 
 commands.brand = {
     help: 'shows info about a splatoon brand',
-    command: function(opts) {
-        var input = opts.words.join(' ');
+    command: function(message) {
+        var input = message.args.join(' ');
         var lower_case_input = input.toLowerCase();
 
         if(lower_case_input == 'list') {
-            bot.sendMessage({ to: opts.chatID, message: config.splatoon.brands.map(function(arg) { return arg.name; }).join(', ') });
+            bot.sendMessage(message.channel, config.splatoon.brands.map(function(arg) { return arg.name; }).join(', '));
             return;
         }
-        var brand = config.splatoon.brands.find(function(arg) {
+        var brand = find_from(config.splatoon.brands, function(arg) {
             return arg.name.toLowerCase() == lower_case_input;
         });
 
         if(!brand) {
-            bot.sendMessage({ to: opts.chatID, message: 'Could not find brand "' + input + '".' });
+            bot.sendMessage(message.channel, 'Could not find brand "' + input + '".');
             return;
         }
 
-        var message = '';
+        var result = '';
 
         if(brand.buffed == null || brand.nerfed == null) {
-            message = 'The brand "' + brand.name + '" is neutral!\n';
+            result = 'The brand "' + brand.name + '" is neutral!\n';
         }
         else {
-            message = 'The brand "' + brand.name + '" has ';
-            message = message.concat('buffed ', config.splatoon.abilities[brand.buffed - 1], ' and nerfed ', config.splatoon.abilities[brand.nerfed - 1], ' probabilities\n');
+            result = 'The brand "' + brand.name + '" has ';
+            result = result.concat('buffed ', config.splatoon.abilities[brand.buffed - 1], ' and nerfed ', config.splatoon.abilities[brand.nerfed - 1], ' probabilities\n');
         }
 
-        bot.sendMessage({ to: opts.chatID, message: message });
+        bot.sendMessage(message.channel, result);
     }
 };
 
 commands.reloadconfig = {
     authority: 3,
-    command: function(opts) {
+    command: function(message) {
         load_config();
     }
 };
 
-function message(user, userID, chatID, message, rawEvent) {
-    console.log(user + ' said: ' + message);
-    var words = message.split(' ');
+commands.info = {
+    help: 'shows information about the current user or another user',
+    command: function(message) {
+        var server = message.channel.server;
+        var user = server.members.filter('username', message.args.join(' '), true) || message.author;
+        var owner = server.members.filter('id', server.ownerID, true).username;
+        var authority = get_user_authority(user.id);
+        var text = 'Info for ' + user.mention() + ':\nYou\'re currently in #' + message.channel.name + ' in server ' + server.name;
+        text = text.concat(' (', server.region, ')\n', 'The owner of this group is ', owner, '\n', 'Your Discord ID is: ', user.id);
+        text = text.concat('\nYour authority on me is **' + authority_prettify[authority] + '**');
+        bot.sendMessage(message.channel, text);
+    }
+};
+
+commands.cleanup = {
+    help: 'cleans up past messages',
+    authority: 1,
+    command: function(message) {
+        var amount = parseInt(message.args[0]) || 100;
+        var text = '';
+        var count = 0;
+        var done;
+        bot.getChannelLogs(message.channel, amount, function(logs) {
+            for(message of logs.contents) {
+                if(message.author.id === bot.user.id) {
+                    ++count;
+                    bot.deleteMessage(message);
+                }
+            }
+            bot.deleteMessage(done);
+            bot.sendMessage(message.channel, 'Cleanup has completed. ' + count + ' messages were deleted', false, true, { selfDestruct: 3000 });
+        });
+        bot.sendMessage(message.channel, 'Cleaning up...', function(arg) { done = arg; });
+    }
+}
+
+commands.authority = {
+    help: 'manages the authority of a user',
+    authority: 1,
+    command: function(message) {
+        var server = message.channel.server;
+        var authority = parseInt(message.args[0]) || 0;
+        var author_authority = get_user_authority(message.author.id);
+        var user = server.members.filter('username', message.args.slice(1).join(' '), true);
+
+        if(!user) {
+            bot.sendMessage(message.channel, 'User not found');
+            return;
+        }
+
+        if(authority > author_authority || author_authority < get_user_authority(user.id)) {
+            bot.sendMessage(message.channel, "You can't give someone authority higher than yours.");
+            return;
+        }
+
+        config.authority[user.id] = authority;
+        save_config();
+        bot.sendMessage(message.channel, user.username + ' now has an authority of **' + authority_prettify[authority] + '**');
+    }
+}
+
+function message_callback(message) {
+    // console.log(user + ' said: ' + message);
+    var words = message.content.split(' ');
     var prefix = words[0];
-    opts = { user: user, userID: userID, message: message, rawEvent: rawEvent, chatID: chatID, words: words.slice(1) };
     var command = get_command_from_message(prefix);
 
     if(command) {
+        message.args = words.slice(1);
         if(typeof(command) == 'object') {
             var authority_required = command.authority || 0;
-            if(get_user_authority(userID) >= authority_required) {
-                command.command(opts);
+            if(get_user_authority(message.author.id) >= authority_required) {
+                command.command(message);
             }
             else {
-                bot.sendMessage({ to: opts.chatID, message: "Sorry, you're not authorised to use this command" });
+                bot.sendMessage(message.channel, "Sorry, you're not authorised to use this command");
             }
         }
         else {
-            command(opts);
+            command(message);
         }
     }
 }
 
-bot.on('message', message);
-bot.on('ready', function(rawEvent) {
+bot.on('message', message_callback);
+bot.on('ready', function() {
     console.log('Connected!\nLogging in as: ');
-    console.log(bot.username);
-    console.log(bot.id);
+    console.log(bot.user.username);
+    console.log(bot.user.id);
     console.log('-----');
 });
