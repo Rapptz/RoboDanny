bf08dfaa3384c67dbaf86f31006c1cea462ae7db
Rapptz <rapptz@gmail.com>
2015-10-02 17:19:09 -0400

    Add welcome message for /r/splatoon chat.

--- bot.py
+++ bot.py
@@ -14,6 +14,14 @@ def on_ready():
 def on_message(message):
     commands.dispatch_messages(bot, message)
 
+@bot.event
+def on_member_join(member):
+    if member.server.id == '86177841854566400':
+        # check if this is /r/Splatoon
+        channel = discord.utils.find(lambda c: c.id == '86177841854566400', member.server.channels)
+        if channel is not None:
+            bot.send_message(channel, 'Welcome {}, to the /r/Splatoon Discord.'.format(member.name))
+
 if __name__ == '__main__':
     commands.load_config()
     bot.login(commands.config['username'], commands.config['password'])
