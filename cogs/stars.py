81aad57252c995af207c21213a2e8de1e206a90e
Rapptz <rapptz@gmail.com>
2016-06-15 06:46:28 -0400

    Bump up newly starred messages.

--- cogs/stars.py
+++ cogs/stars.py
@@ -255,7 +255,12 @@ class Stars:
             await self.stars.put(guild_id, db)
             return
 
-        await self.bot.edit_message(bot_msg, to_send)
+        # delete and recreate the message
+        # this 'bumps' it up.
+        await self.bot.delete_message(bot_msg)
+        sent = await self.bot.send_message(starboard, to_send)
+        stars[0] = sent.id
+
         await self.stars.put(guild_id, db)
 
     @star.error
