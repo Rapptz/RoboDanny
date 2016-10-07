ed2809ce0331db30f1c932289bc52bbea7ca777b
Rapptz <rapptz@gmail.com>
2016-10-06 22:32:30 -0400

    Add ?g command to quickly google something.

--- cogs/buttons.py
+++ cogs/buttons.py
@@ -2,6 +2,9 @@ from discord.ext import commands
 from datetime import datetime
 import discord
 from .utils import checks
+import aiohttp
+from urllib.parse import parse_qs
+from lxml import etree
 
 def date(argument):
     formats = (
@@ -69,6 +72,74 @@ class Buttons:
     async def nostalgia_error(self, error, ctx):
         if type(error) is commands.BadArgument:
             await self.bot.say(error)
+
+    async def get_google_entries(self, query):
+        params = {
+            'q': query,
+            'safe': 'on'
+        }
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64)'
+        }
+
+        # list of URLs
+        entries = []
+
+        async with aiohttp.get('https://www.google.com/search', params=params, headers=headers) as resp:
+            if resp.status != 200:
+                raise RuntimeError('Google somehow failed to respond.')
+
+            root = etree.fromstring(await resp.text(), etree.HTMLParser())
+
+            """
+            Tree looks like this.. sort of..
+
+            <div class="g">
+                ...
+                <h3>
+                    <a href="/url?q=<url>" ...>title</a>
+                </h3>
+                ...
+                <span class="st">
+                    <span class="f">date here</span>
+                    summary here, can contain <em>tag</em>
+                </span>
+            </div>
+            """
+
+            search_nodes = root.findall(".//div[@class='g']")
+            for node in search_nodes:
+                url_node = node.find('.//h3/a')
+                if url_node is None:
+                    continue
+
+                url = url_node.attrib['href']
+                if not url.startswith('/url?'):
+                    continue
+
+                url = parse_qs(url[5:])['q'][0] # get the URL from ?q query string
+
+                # if I ever cared about the description, this is how
+                entries.append(url)
+
+                # short = node.find(".//span[@class='st']")
+                # if short is None:
+                #     entries.append((url, ''))
+                # else:
+                #     text = ''.join(short.itertext())
+                #     entries.append((url, text.replace('...', '')))
+
+        return entries
+
+    @commands.command(aliases=['google'])
+    async def g(self, *, query):
+        """Searches google and gives you top result."""
+        try:
+            entries = await self.get_google_entries(query)
+        except RuntimeError as e:
+            await self.bot.say(str(e))
+        else:
+            await self.bot.say(entries[0])
 
 def setup(bot):
     bot.add_cog(Buttons(bot))
