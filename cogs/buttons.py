0fa05213a49f76bf1ec85fe427e28477f5142345
PapyrusThePlant <papyrustheplant@gmail.com>
2016-12-30 09:48:49 +0100

    Fix user-agent to receive translation cards in `?g`.

--- cogs/buttons.py
+++ cogs/buttons.py
@@ -238,7 +238,7 @@ class Buttons:
             'safe': 'on'
         }
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64)'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
         }
 
         # list of URLs
