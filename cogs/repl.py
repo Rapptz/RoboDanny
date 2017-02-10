de5a46612fd7847ba13261e62c34378924c81567
Rapptz <rapptz@gmail.com>
2017-02-10 00:03:59 -0500

    Add a nicer to use eval command.

--- cogs/repl.py
+++ cogs/repl.py
@@ -4,12 +4,14 @@ import asyncio
 import traceback
 import discord
 import inspect
+import textwrap
 from contextlib import redirect_stdout
 import io
 
 class REPL:
     def __init__(self, bot):
         self.bot = bot
+        self._last_result = None
         self.sessions = set()
 
     def cleanup_code(self, content):
@@ -23,6 +25,52 @@ class REPL:
 
     def get_syntax_error(self, e):
         return '```py\n{0.text}{1:>{0.offset}}\n{2}: {0}```'.format(e, '^', type(e).__name__)
+
+    @commands.command(pass_context=True, hidden=True, name='eval')
+    @checks.is_owner()
+    async def _eval(self, ctx, *, body: str):
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'channel': ctx.message.channel,
+            'author': ctx.message.author,
+            'server': ctx.message.server,
+            'message': ctx.message,
+            '_': self._last_result
+        }
+
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+
+        to_compile = 'async def func():\n%s' % textwrap.indent(body, '  ')
+
+        try:
+            exec(to_compile, env)
+        except SyntaxError as e:
+            return await self.bot.say(self.get_syntax_error(e))
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await self.bot.say('```py\n{}{}\n```'.format(value, traceback.format_exc()))
+        else:
+            value = stdout.getvalue()
+            try:
+                await self.bot.add_reaction(ctx.message, '\u2705')
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    await self.bot.say('```py\n%s\n```' % value)
+            else:
+                self._last_result = ret
+                await self.bot.say('```py\n%s%s\n```' % (value, ret))
 
     @commands.command(pass_context=True, hidden=True)
     @checks.is_owner()
