45b9f14c4e1b9589831000775f2998d427bba124
Rapptz <rapptz@gmail.com>
2017-05-05 20:30:26 -0400

    Fix blobsort.

--- cogs/emoji.py
+++ cogs/emoji.py
@@ -299,7 +299,7 @@ class Emoji:
             fmt = 'Page %s\n' % number
             fp.write(fmt.encode('utf-8'))
             for emoji in page:
-                fmt = ':{0.name}: = `:{0.name}:`\n'.format(emoji)
+                fmt = ':{0}: = `:{0}:`\n'.format(emoji)
                 fp.write(fmt.encode('utf-8'))
 
             fp.write(b'\n')
