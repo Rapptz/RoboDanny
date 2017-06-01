ce767020fbbcbd889a541d2f6c771db5c50b2524
Rapptz <rapptz@gmail.com>
2017-05-31 22:52:06 -0400

    Convert carbonitex cog.

--- cogs/carbonitex.py
+++ cogs/carbonitex.py
@@ -2,7 +2,7 @@ import aiohttp
 import json
 import logging
 
-log = logging.getLogger()
+log = logging.getLogger(__name__)
 
 CARBONITEX_API_BOTDATA = 'https://www.carbonitex.net/discord/data/botdata.php'
 DISCORD_BOTS_API       = 'https://bots.discord.pw/api'
@@ -11,23 +11,19 @@ class Carbonitex:
     """Cog for updating carbonitex.net and bots.discord.pw bot information."""
     def __init__(self, bot):
         self.bot = bot
-        self.session = aiohttp.ClientSession()
-
-    def __unload(self):
-        # pray it closes
-        self.bot.loop.create_task(self.session.close())
 
     async def update(self):
+        guild_count = len(self.bot.guilds)
         carbon_payload = {
             'key': self.bot.carbon_key,
-            'servercount': len(self.bot.servers)
+            'servercount': guild_count
         }
 
-        async with self.session.post(CARBONITEX_API_BOTDATA, data=carbon_payload) as resp:
+        async with self.bot.session.post(CARBONITEX_API_BOTDATA, data=carbon_payload) as resp:
             log.info('Carbon statistics returned {0.status} for {1}'.format(resp, carbon_payload))
 
         payload = json.dumps({
-            'server_count': len(self.bot.servers)
+            'server_count': guild_count
         })
 
         headers = {
@@ -36,13 +32,13 @@ class Carbonitex:
         }
 
         url = '{0}/bots/{1.user.id}/stats'.format(DISCORD_BOTS_API, self.bot)
-        async with self.session.post(url, data=payload, headers=headers) as resp:
+        async with self.bot.session.post(url, data=payload, headers=headers) as resp:
             log.info('DBots statistics returned {0.status} for {1}'.format(resp, payload))
 
-    async def on_server_join(self, server):
+    async def on_guild_join(self, guild):
         await self.update()
 
-    async def on_server_remove(self, server):
+    async def on_guild_remove(self, guild):
         await self.update()
 
     async def on_ready(self):
