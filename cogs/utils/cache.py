8e3902a95933991e1ffbd34080f5d24e31442365
Rapptz <rapptz@gmail.com>
2017-06-17 20:56:07 -0400

    Have a better cache key for lru_cache decorator.

--- cogs/utils/cache.py
+++ cogs/utils/cache.py
@@ -23,12 +23,32 @@ def lru_cache(maxsize=128):
     def decorator(func):
         _internal_cache = LRU(maxsize)
 
-        def _make_key(*args, **kwargs):
-            return f'{func.__module__}.{func.__name__}#{repr(args)}##{repr(kwargs)}'
+        def _make_key(args, kwargs):
+            # this is a bit of a cluster fuck
+            # we do care what 'self' parameter is when we __repr__ it
+            def _true_repr(o):
+                if o.__class__.__repr__ is object.__repr__:
+                    return f'<{o.__class__.__module__}.{o.__class__.__name__}>'
+                return repr(o)
+
+            key = [ f'{func.__module__}.{func.__name__}' ]
+            key.extend(_true_repr(o) for o in args)
+            for k, v in kwargs.items():
+                # note: this only really works for this use case in particular
+                # I want to pass asyncpg.Connection objects to the parameters
+                # however, they use default __repr__ and I do not care what
+                # connection is passed in, so I needed a bypass.
+                if k == 'connection':
+                    continue
+
+                key.append(_true_repr(k))
+                key.append(_true_repr(v))
+
+            return ''.join(key)
 
         @wraps(func)
         def wrapper(*args, **kwargs):
-            key = _make_key(*args, **kwargs)
+            key = _make_key(args, kwargs)
             try:
                 value = _internal_cache[key]
             except KeyError:
@@ -46,12 +66,14 @@ def lru_cache(maxsize=128):
 
         def _invalidate(*args, **kwargs):
             try:
-                del _internal_cache[_make_key(*args, **kwargs)]
+                del _internal_cache[_make_key(args, kwargs)]
             except KeyError:
-                pass
+                return False
+            else:
+                return True
 
         wrapper.cache = _internal_cache
-        wrapper.get_key = _make_key
+        wrapper.get_key = lambda *args, **kwargs: _make_key(args, kwargs)
         wrapper.invalidate = _invalidate
         return wrapper
     return decorator
