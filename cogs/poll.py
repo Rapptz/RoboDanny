6e8fd070d09000f5712c942a4c6a04ff1b4a7e86
Rapptz <rapptz@gmail.com>
2017-07-12 22:49:31 -0400

    Use regional indicators for polls instead of key caps.

--- cogs/poll.py
+++ cogs/poll.py
@@ -1,8 +1,9 @@
 from discord.ext import commands
 import asyncio
 
-def to_keycap(c):
-    return '\N{KEYCAP TEN}' if c == 10 else str(c) + '\u20e3'
+def to_emoji(c):
+    base = 0x1f1e6
+    return chr(base + c)
 
 class Polls:
     """Poll voting system."""
@@ -25,9 +26,8 @@ class Polls:
         def check(m):
             return m.author == ctx.author and m.channel == ctx.channel and len(m.content) <= 100
 
-        for i in range(1, 11):
+        for i in range(20):
             messages.append(await ctx.send(f'Say poll option or {ctx.prefix}cancel to publish poll.'))
-
 
             try:
                 entry = await self.bot.wait_for('message', check=check, timeout=60.0)
@@ -39,7 +39,7 @@ class Polls:
             if entry.clean_content.startswith(f'{ctx.prefix}cancel'):
                 break
 
-            answers.append((to_keycap(i), entry.clean_content))
+            answers.append((to_emoji(i), entry.clean_content))
 
         try:
             await ctx.channel.delete_messages(messages)
@@ -66,22 +66,23 @@ class Polls:
 
         if len(questions_and_choices) < 3:
             return await ctx.send('Need at least 1 question with 2 choices.')
-        elif len(questions_and_choices) > 11:
-            return await ctx.send('You can only have up to 10 choices.')
+        elif len(questions_and_choices) > 21:
+            return await ctx.send('You can only have up to 20 choices.')
 
-        perms = ctx.channel.permissions_for(ctx..me)
+        perms = ctx.channel.permissions_for(ctx.me)
         if not (perms.read_message_history or perms.add_reactions):
             return await ctx.send('Need Read Message History and Add Reactions permissions.')
 
         question = questions_and_choices[0]
-        choices = [(to_keycap(e), v) for e, v in enumerate(questions_and_choices[1:], 1)]
+        choices = [(to_emoji(e), v) for e, v in enumerate(questions_and_choices[1:])]
 
         try:
             await ctx.message.delete()
         except:
             pass
 
-        poll = await ctx.send(f'{ctx.author} asks: {question}\n\n{"\n".join(f"{key}: {c}" for key, c in choices)}')
+        body = "\n".join(f"{key}: {c}" for key, c in choices)
+        poll = await ctx.send(f'{ctx.author} asks: {question}\n\n{body}')
         for emoji, _ in choices:
             await poll.add_reaction(emoji)
 
