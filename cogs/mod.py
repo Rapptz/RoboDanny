1e68b82dcefbd05ad03b11387a8f3f01cc43ddc5
Rapptz <rapptz@gmail.com>
2017-07-18 06:15:17 -0400

    Reason maximum is 512 not 256.

--- cogs/mod.py
+++ cogs/mod.py
@@ -74,8 +74,8 @@ class ActionReason(commands.Converter):
     async def convert(self, ctx, argument):
         ret = f'{ctx.author} (ID: {ctx.author.id}): {argument}'
 
-        if len(ret) > 256:
-            reason_max = 256 - len(ret) - len(argument)
+        if len(ret) > 512:
+            reason_max = 512 - len(ret) - len(argument)
             raise commands.BadArgument(f'reason is too long ({len(argument)}/{reason_max})')
         return ret
 
