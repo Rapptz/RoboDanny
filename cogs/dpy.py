38cf5ed3caa13abbd279b26c7c94face15788da4
Rapptz <rapptz@gmail.com>
2017-11-26 16:16:39 -0500

    Add issue hotlinking to the d.py server

--- cogs/dpy.py
+++ cogs/dpy.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import discord
+import re
 
 DISCORD_PY_GUILD_ID = 336642139381301249
 DISCORD_PY_BOTS_ROLE = 381980817125015563
@@ -8,6 +9,7 @@ DISCORD_PY_REWRITE_ROLE = 381981861041143808
 class DPYExclusive:
     def __init__(self, bot):
         self.bot = bot
+        self.issue = re.compile(r'##(?P<number>[0-9]+)')
 
     async def on_member_join(self, member):
         if member.guild.id != DISCORD_PY_GUILD_ID:
@@ -15,6 +17,15 @@ class DPYExclusive:
 
         if member.bot:
             await member.add_roles(discord.Object(id=DISCORD_PY_BOTS_ROLE))
+
+    async def on_message(self, message):
+        if not message.guild or message.guild.id != DISCORD_PY_GUILD_ID:
+            return
+
+        m = self.issue.search(message.content)
+        if m is not None:
+            url = 'https://github.com/Rapptz/discord.py/issues/'
+            await message.channel.send(url + m.group('number'))
 
     @commands.command(hidden=True)
     @commands.check(lambda ctx: ctx.guild and ctx.guild.id == DISCORD_PY_GUILD_ID)
