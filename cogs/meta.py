8919ec0a455f957848ef77b479fe3494e76f0aa7
Rapptz <rapptz@gmail.com>
2018-02-11 03:37:29 -0500

    Make ?charinfo terminate based on number of characters in output.

--- cogs/meta.py
+++ cogs/meta.py
@@ -61,15 +61,14 @@ class Meta:
         Only up to 25 characters at a time.
         """
 
-        if len(characters) > 25:
-            return await ctx.send(f'Too many characters ({len(characters)}/25)')
-
         def to_string(c):
             digit = f'{ord(c):x}'
             name = unicodedata.name(c, 'Name not found.')
             return f'`\\U{digit:>08}`: {name} - {c} \N{EM DASH} <http://www.fileformat.info/info/unicode/char/{digit}>'
-
-        await ctx.send('\n'.join(map(to_string, characters)))
+        msg = '\n'.join(map(to_string, characters))
+        if len(msg) > 2000:
+            return await ctx.send('Output too long to display.')
+        await ctx.send(msg)
 
     @commands.group(name='prefix', invoke_without_command=True)
     async def prefix(self, ctx):
