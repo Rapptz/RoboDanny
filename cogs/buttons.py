7003492cc71be21134b99dd99bfe73c13a1fa4f4
Rapptz <rapptz@gmail.com>
2018-04-02 19:05:48 -0400

    Add ?spoiler command to show spoilers

--- cogs/buttons.py
+++ cogs/buttons.py
@@ -6,6 +6,8 @@ from lxml import etree
 import random
 import logging
 from urllib.parse import quote as uriquote
+from lru import LRU
+import io
 
 log = logging.getLogger(__name__)
 
@@ -23,11 +25,85 @@ def date(argument):
 
     raise commands.BadArgument('Cannot convert to date. Expected YYYY/MM/DD or YYYY-MM-DD.')
 
+def can_use_spoiler():
+    def predicate(ctx):
+        if ctx.guild is None:
+            raise commands.BadArgument('Cannot be used in private messages.')
+
+        my_permissions = ctx.channel.permissions_for(ctx.guild.me)
+        if not (my_permissions.read_message_history and my_permissions.manage_messages and my_permissions.add_reactions):
+            raise commands.BadArgument('Need Read Message History, Add Reactions and Manage Messages ' \
+                                       'to permission to use this. Sorry if I spoiled you.')
+        return True
+    return commands.check(predicate)
+
+SPOILER_EMOJI_ID = 430469957042831371
+
+class SpoilerCache:
+    __slots__ = ('author_id', 'channel_id', 'title', 'text', 'attachments')
+
+    def __init__(self, data):
+        self.author_id = data['author_id']
+        self.channel_id = data['channel_id']
+        self.title = data['title']
+        self.text = data['text']
+        self.attachments = data['attachments']
+
+    def has_single_image(self):
+        return self.attachments and self.attachments[0].filename.lower().endswith(('.gif', '.png', '.jpg', '.jpeg'))
+
+    def to_embed(self, bot):
+        embed = discord.Embed(title=f'{self.title} Spoiler', colour=0x01AEEE)
+        if self.text:
+            embed.description = self.text
+
+        if self.has_single_image():
+            if self.text is None:
+                embed.title = f'{self.title} Spoiler Image'
+            embed.set_image(url=self.attachments[0].url)
+            attachments = self.attachments[1:]
+        else:
+            attachments = self.attachments
+
+        if attachments:
+            value = '\n'.join(f'[{a.filename}]({a.url})' for a in attachments)
+            embed.add_field(name='Attachments', value=value, inline=False)
+
+        user = bot.get_user(self.author_id)
+        if user:
+            embed.set_author(name=str(user), icon_url=user.avatar_url_as(format='png'))
+
+        return embed
+
+    def to_spoiler_embed(self, ctx, storage_message):
+        description = 'React with <:spoiler:430469957042831371> to reveal the spoiler.'
+        embed = discord.Embed(title=f'{self.title} Spoiler', description=description)
+        if self.has_single_image() and self.text is None:
+            embed.title = f'{self.title} Spoiler Image'
+
+        embed.set_footer(text=storage_message.id)
+        embed.colour = 0x01AEEE
+        embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url_as(format='png'))
+        return embed
+
+class SpoilerCooldown(commands.CooldownMapping):
+    def __init__(self):
+        super().__init__(commands.Cooldown(1, 60.0, commands.BucketType.user))
+
+    def _bucket_key(self, user_id):
+        return user_id
+
+    def is_rate_limited(self, user_id):
+        bucket = self.get_bucket(user_id)
+        return bucket.update_rate_limit() is not None
+
 class Buttons:
     """Buttons that make you feel."""
 
     def __init__(self, bot):
         self.bot = bot
+        self._spoiler_cache = LRU(128)
+        self._spoiler_cooldown = SpoilerCooldown()
 
     @commands.command(hidden=True)
     async def feelgood(self, ctx):
@@ -445,6 +521,139 @@ class Buttons:
             await ctx.send(f'Could not PM user by ID {user_id}.')
         else:
             await ctx.send('PM successfully sent.')
+
+    async def redirect_post(self, ctx, title, text):
+        storage = self.bot.get_guild(182325885867786241).get_channel(430229522340773899)
+
+        supported_attachments = ('.png', '.jpg', '.jpeg', '.webm', '.gif', '.mp4', '.txt')
+        if not all(attach.filename.lower().endswith(supported_attachments) for attach in ctx.message.attachments):
+            raise RuntimeError(f'Unsupported file in attachments. Only {", ".join(supported_attachments)} supported.')
+
+        files = []
+        total_bytes = 0
+        eight_mib = 8 * 1024 * 1024
+        for attach in ctx.message.attachments:
+            async with ctx.session.get(attach.url) as resp:
+                if resp.status != 200:
+                    continue
+
+                content_length = int(resp.headers.get('Content-Length'))
+
+                # file too big, skip it
+                if (total_bytes + content_length) > eight_mib:
+                    continue
+
+                total_bytes += content_length
+                fp = io.BytesIO(await resp.read())
+                files.append(discord.File(fp, filename=attach.filename))
+
+            if total_bytes >= eight_mib:
+                break
+
+        data = discord.Embed(title=title)
+        if text:
+            data.description = text
+
+        data.set_author(name=ctx.author.id)
+        data.set_footer(text=ctx.channel.id)
+
+        try:
+            message = await storage.send(embed=data, files=files)
+        except discord.HTTPException as e:
+            raise RuntimeError(f'Sorry. Could not store message due to {e.__class__.__name__}: {e}.') from e
+
+        to_dict = {
+            'author_id': ctx.author.id,
+            'channel_id': ctx.channel.id,
+            'attachments': message.attachments,
+            'title': title,
+            'text': text
+        }
+
+        cache = SpoilerCache(to_dict)
+        return message, cache
+
+    async def get_spoiler_cache(self, channel_id, message_id):
+        try:
+            return self._spoiler_cache[message_id]
+        except KeyError:
+            pass
+
+        storage = self.bot.get_guild(182325885867786241).get_channel(430229522340773899)
+
+        # slow path requires 2 lookups
+        # first is looking up the message_id of the original post
+        # to get the embed footer information which points to the storage message ID
+        # the second is getting the storage message ID and extracting the information from it
+        channel = self.bot.get_channel(channel_id)
+        if not channel:
+            return None
+
+        try:
+            original_message = await channel.get_message(message_id)
+            storage_message_id = int(original_message.embeds[0].footer.text)
+            message = await storage.get_message(storage_message_id)
+        except:
+            # this message is probably not the proper format or the storage died
+            return None
+
+        data = message.embeds[0]
+        to_dict = {
+            'author_id': int(data.author.name),
+            'channel_id': int(data.footer.text),
+            'attachments': message.attachments,
+            'title': data.title,
+            'text': None if not data.description else data.description
+        }
+        cache = SpoilerCache(to_dict)
+        self._spoiler_cache[message_id] = cache
+        return cache
+
+    async def on_raw_reaction_add(self, emoji, message_id, channel_id, user_id):
+        if emoji.id != SPOILER_EMOJI_ID:
+            return
+
+        user = self.bot.get_user(user_id)
+        if not user or user.bot:
+            return
+
+        if self._spoiler_cooldown.is_rate_limited(user_id):
+            return
+
+        cache = await self.get_spoiler_cache(channel_id, message_id)
+        embed = cache.to_embed(self.bot)
+        await user.send(embed=embed)
+
+    @commands.command()
+    @can_use_spoiler()
+    async def spoiler(self, ctx, title, *, text=None):
+        """Marks your post a spoiler with a title.
+
+        Once your post is marked as a spoiler it will be
+        automatically deleted and the bot will DM those who
+        opt-in to view the spoiler.
+
+        The only media types supported are png, gif, jpeg, mp4,
+        and webm.
+
+        Only 8MiB of total media can be uploaded at once.
+        Sorry, Discord limitation.
+
+        To opt-in to a post's spoiler you must click the reaction.
+        """
+
+        if len(title) > 100:
+            return await ctx.send('Sorry. Title has to be shorter than 100 characters.')
+
+        try:
+            storage_message, cache = await self.redirect_post(ctx, title, text)
+        except Exception as e:
+            return await ctx.send(str(e))
+
+        await ctx.message.delete()
+        spoiler_message = await ctx.send(embed=cache.to_spoiler_embed(ctx, storage_message))
+        self._spoiler_cache[spoiler_message.id] = cache
+        await spoiler_message.add_reaction(':spoiler:430469957042831371')
 
 def setup(bot):
     bot.add_cog(Buttons(bot))
