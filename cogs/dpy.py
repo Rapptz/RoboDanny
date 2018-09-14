489fae9082f60baa7950a8e1cca7eb99fd20731e
Rapptz <rapptz@gmail.com>
2018-09-14 18:53:38 -0400

    Invite tracking for Japanese users in discord.py

--- cogs/dpy.py
+++ cogs/dpy.py
@@ -5,11 +5,22 @@ import re
 DISCORD_PY_GUILD_ID = 336642139381301249
 DISCORD_PY_BOTS_ROLE = 381980817125015563
 DISCORD_PY_REWRITE_ROLE = 381981861041143808
+DISCORD_PY_JP_ROLE = 490286873311182850
 
 class DPYExclusive:
     def __init__(self, bot):
         self.bot = bot
         self.issue = re.compile(r'##(?P<number>[0-9]+)')
+        self._invite_cache = {}
+        self.bot.loop.create_task(self._prepare_invites())
+
+    async def _prepare_invites(self):
+        await self.bot.wait_until_ready()
+        guild = self.bot.get_guild(DISCORD_PY_GUILD_ID)
+        invites = await guild.invites()
+        self._invite_cache = {
+            invite.code: invite.uses
+        }
 
     async def on_member_join(self, member):
         if member.guild.id != DISCORD_PY_GUILD_ID:
@@ -17,6 +28,14 @@ class DPYExclusive:
 
         if member.bot:
             await member.add_roles(discord.Object(id=DISCORD_PY_BOTS_ROLE))
+            return
+
+        JP_INVITE_CODE = 'nXzj3dg'
+        invites = await member.guild.invites()
+        for invite in invites:
+            if invite.code == JP_INVITE_CODE and invite.uses > self._invite_cache[invite.code]:
+                await member.add_roles(discord.Object(id=DISCORD_PY_JP_ROLE))
+            self._invite_cache[invite.code] = invite.uses
 
     async def on_message(self, message):
         if not message.guild or message.guild.id != DISCORD_PY_GUILD_ID:
