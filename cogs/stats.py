2e178eb575119a23e79f0541264deef30619f747
Rapptz <rapptz@gmail.com>
2019-03-21 17:11:24 -0400

    Add bot health commands.

--- cogs/stats.py
+++ cogs/stats.py
@@ -12,6 +12,8 @@ import asyncio
 import psutil
 import os
 import re
+import io
+import gc
 
 log = logging.getLogger(__name__)
 
@@ -31,6 +33,15 @@ _INVITE_REGEX = re.compile(r'(?:https?:\/\/)?discord(?:\.gg|\.com|app\.com\/invi
 
 def censor_invite(obj, *, _regex=_INVITE_REGEX):
     return _regex.sub('[censored-invite]', str(obj))
+
+def hex_value(arg):
+    return int(arg, base=16)
+
+def object_at(addr):
+    for o in gc.get_objects():
+        if id(o) == addr:
+            return o
+    return None
 
 class Stats(commands.Cog):
     """Bot usage statistics."""
@@ -577,6 +588,127 @@ class Stats(commands.Cog):
         e.description = f'```py\n{exc}\n```'
         e.timestamp = datetime.datetime.utcnow()
         await self.webhook.send(embed=e)
+
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def bothealth(self, ctx):
+        """Various bot health monitoring tools."""
+
+        # This uses a lot of private methods because there is no
+        # clean way of doing this otherwise.
+
+        HEALTHY = discord.Colour(value=0x43B581)
+        UNHEALTHY = discord.Colour(value=0xF04947)
+        WARNING = discord.Colour(value=0xF09E47)
+        total_warnings = 0
+
+        embed = discord.Embed(title='Bot Health Report', colour=HEALTHY)
+
+        # Check the connection pool health.
+        pool = self.bot.pool
+        total_waiting = len(pool._queue._getters)
+        current_generation = pool._generation
+
+        description = [
+            f'Total `Pool.acquire` Waiters: {total_waiting}',
+            f'Current Pool Generation: {current_generation}'
+        ]
+
+        questionable_connections = 0
+        connection_value = []
+        for index, holder in enumerate(pool._queue._queue, start=1):
+            generation = holder._generation
+            in_use = holder._in_use is not None
+            is_closed = holder._con is None or holder._con.is_closed()
+            display = f'generation={holder._generation} in_use={in_use} closed={is_closed}'
+            questionable_connections += any((in_use, generation != current_generation))
+            connection_value.append(f'<Holder i={index} {display}>')
+
+        joined_value = '\n'.join(connection_value)
+        embed.add_field(name='Connections', value=f'```py\n{joined_value}\n```', inline=False)
+
+        spam_control = self.bot.spam_control
+        being_spammed = [
+            str(key) for key, value in spam_control._cache.items()
+            if value._tokens == 0
+        ]
+
+        description.append(f'Current Spammers: {", ".join(being_spammed) if being_spammed else "None"}')
+        description.append(f'Questionable Connections: {questionable_connections}')
+
+        total_warnings += questionable_connections
+        total_warnings += bool(being_spammed)
+
+        try:
+            task_retriever = asyncio.Task.all_tasks
+        except AttributeError:
+            # future proofing for 3.9 I guess
+            task_retriever = asyncio.all_tasks
+        else:
+            all_tasks = task_retriever(loop=self.bot.loop)
+
+        event_tasks = [
+            t for t in all_tasks
+            if 'Client._run_event' in repr(t) and not t.done()
+        ]
+
+        cogs_directory = os.path.dirname(__file__)
+
+        inner_tasks = [
+            t for t in all_tasks
+            if cogs_directory in repr(t)
+        ]
+
+        bad_inner_tasks = ", ".join(hex(id(t)) for t in inner_tasks if t.done() and t._exception is not None)
+        total_warnings += bool(bad_inner_tasks)
+        embed.add_field(name='Inner Tasks', value=f'Total: {len(inner_tasks)}\nFailed: {bad_inner_tasks or "None"}')
+
+        task_ids = tuple(hex(id(t)) for t in event_tasks)
+        embed.add_field(name='Events Waiting', value=f'Total: {len(task_ids)}\n{", ".join(task_ids)}', inline=False)
+
+        command_waiters = len(self._data_batch)
+        is_locked = self._batch_lock.locked()
+        description.append(f'Commands Waiting: {command_waiters}, Batch Locked: {is_locked}')
+
+        memory_usage = self.process.memory_full_info().uss / 1024**2
+        cpu_usage = self.process.cpu_percent() / psutil.cpu_count()
+        embed.add_field(name='Process', value=f'{memory_usage:.2f} MiB\n{cpu_usage:.2f}% CPU')
+
+        if command_waiters >= 8:
+            total_warnings += 1
+
+        if total_warnings >= 8:
+            embed.colour = UNHEALTHY
+        elif total_warnings >= 2:
+            embed.colour = WARNING
+
+        embed.set_footer(text=f'{total_warnings} warning(s)')
+        embed.description = '\n'.join(description)
+        await ctx.send(embed=embed)
+
+    @commands.command(hidden=True, aliases=['cancel_task'])
+    @commands.is_owner()
+    async def debug_task(self, ctx, memory_id: hex_value):
+        """Debug a task by a memory location."""
+        task = object_at(memory_id)
+        if task is None:
+            return await ctx.send(f'Could not find object at {hex(memory_id)}.')
+
+        if ctx.invoked_with == 'cancel_task':
+            task.cancel()
+            return await ctx.send(f'Cancelled task object {task!r}.')
+
+        paginator = commands.Paginator(prefix='```py')
+        fp = io.StringIO()
+        frames = len(task.get_stack())
+        paginator.add_line(f'# Total Frames: {frames}')
+        task.print_stack(file=fp)
+
+        for line in fp.getvalue().splitlines():
+            paginator.add_line(line)
+
+        for page in paginator.pages:
+            await ctx.send(page)
 
 old_on_error = commands.Bot.on_error
 
