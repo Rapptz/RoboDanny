b76ea6f44434b722afc5121ac9dc4afcf757a0ef
Rapptz <rapptz@gmail.com>
2019-04-09 04:47:55 -0400

    Fix XPath for ?faq

--- cogs/api.py
+++ cogs/api.py
@@ -588,7 +588,7 @@ class API(commands.Cog):
             text = await resp.text(encoding='utf-8')
 
             root = etree.fromstring(text, etree.HTMLParser())
-            nodes = root.findall(".//div[@id='questions']/ul[@class='simple']//ul/li/a")
+            nodes = root.findall(".//div[@id='questions']/ul[@class='simple']/li/ul//a")
             for node in nodes:
                 self.faq_entries[''.join(node.itertext()).strip()] = base_url + node.get('href').strip()
 
