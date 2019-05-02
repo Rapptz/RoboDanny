b36b133453dee615e318df11eca1454ab6d3a274
Rapptz <rapptz@gmail.com>
2019-05-01 22:42:17 -0400

    Add ?reload all to pull from git and reload updated modules.

--- cogs/admin.py
+++ cogs/admin.py
@@ -4,9 +4,12 @@ import traceback
 import discord
 import inspect
 import textwrap
+import importlib
 from contextlib import redirect_stdout
 import io
 import os
+import re
+import sys
 import copy
 import subprocess
 from typing import Union
@@ -72,7 +75,7 @@ class Admin(commands.Cog):
         else:
             await ctx.send('\N{OK HAND SIGN}')
 
-    @commands.command(name='reload', hidden=True)
+    @commands.group(name='reload', hidden=True, invoke_without_command=True)
     async def _reload(self, ctx, *, module):
         """Reloads a module."""
         try:
@@ -81,6 +84,70 @@ class Admin(commands.Cog):
             await ctx.send(f'{e.__class__.__name__}: {e}')
         else:
             await ctx.send('\N{OK HAND SIGN}')
+
+    _GIT_PULL_REGEX = re.compile(r'\s*(?P<filename>.+?)\s*\|\s*[0-9]+\s*[+-]+')
+
+    def find_modules_from_git(self, output):
+        files = self._GIT_PULL_REGEX.findall(output)
+        ret = []
+        for file in files:
+            root, ext = os.path.splitext(file)
+            if ext != '.py':
+                continue
+
+            if root.startswith('cogs/'):
+                # A submodule is a directory inside the main cog directory for
+                # my purposes
+                ret.append((root.count('/') - 1, root.replace('/', '.')))
+
+        # For reload order, the submodules should be reloaded first
+        ret.sort(reverse=True)
+        return ret
+
+    @_reload.command(name='all', hidden=True)
+    async def _reload_all(self, ctx):
+        """Reloads all modules, while pulling from git."""
+
+        async with ctx.typing():
+            stdout, stderr = await self.run_process('git pull')
+
+        # progress and stuff is redirected to stderr in git pull
+        # however, things like "fast forward" and files
+        # along with the text "already up-to-date" are in stdout
+
+        if stdout.startswith('Already up-to-date.'):
+            return await ctx.send(stdout)
+
+        modules = self.find_modules_from_git(stdout)
+        mods_text = '\n'.join(f'{index}. `{module}`' for index, (_, module) in enumerate(modules, start=1))
+        prompt_text = f'This will update the following modules, are you sure?\n{mods_text}'
+        confirm = await ctx.prompt(prompt_text, reacquire=False)
+        if not confirm:
+            return await ctx.send('Aborting.')
+
+        statuses = []
+        for is_submodule, module in modules:
+            if is_submodule:
+                try:
+                    actual_module = sys.modules[module]
+                except KeyError:
+                    statuses.append(('<:greyTick:563231201280917524>', module))
+                else:
+                    try:
+                        importlib.reload(actual_module)
+                    except Exception as e:
+                        statuses.append((ctx.tick(False), module))
+                    else:
+                        statuses.append((ctx.tick(True), module))
+            else:
+                try:
+                    self.bot.reload_extension(module)
+                except commands.ExtensionError:
+                    statuses.append((ctx.tick(False), module))
+                else:
+                    statuses.append((ctx.tick(True), module))
+
+        await ctx.send('\n'.join(f'{status}: `{module}`' for status, module in statuses))
 
     @commands.command(pass_context=True, hidden=True, name='eval')
     async def _eval(self, ctx, *, body: str):
