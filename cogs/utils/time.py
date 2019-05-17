b8c427ad97372cb47f16397ff04a6b80e2494757
Rapptz <rapptz@gmail.com>
2019-05-17 01:06:29 -0400

    Revert back to previous implementation of human_timedelta

--- cogs/utils/time.py
+++ cogs/utils/time.py
@@ -172,35 +172,50 @@ def human_timedelta(dt, *, source=None, accuracy=3, brief=False, suffix=True):
     # Microsecond free zone
     now = now.replace(microsecond=0)
     dt = dt.replace(microsecond=0)
+
+    # This implementation uses relativedelta instead of the much more obvious
+    # divmod approach with seconds because the seconds approach is not entirely
+    # accurate once you go over 1 week in terms of accuracy since you have to
+    # hardcode a month as 30 or 31 days.
+    # A query like "11 months" can be interpreted as "!1 months and 6 days"
     if dt > now:
-        seconds = int((dt - now).total_seconds())
+        delta = relativedelta(dt, now)
         suffix = ''
     else:
-        seconds = int((now - dt).total_seconds())
+        delta = relativedelta(now, dt)
         suffix = ' ago' if suffix else ''
 
-    day = 60 * 60 * 24
-    periods = [
-        ('year', 'y', day * 365),
-        ('month', 'mo', day * 30),
-        ('week', 'w', day * 7),
-        ('day', 'd', day),
-        ('hour', 'h', 60 * 60),
-        ('minute', 'm', 60),
-        ('second', 's', 1)
+    attrs = [
+        ('year', 'y'),
+        ('month', 'mo'),
+        ('day', 'd'),
+        ('hour', 'h'),
+        ('minute', 'm'),
+        ('second', 's'),
     ]
 
     output = []
-    for name, brief_name, period in periods:
-        if seconds >= period:
-            value, seconds = divmod(seconds, period)
-            if value == 0:
-                continue
+    for attr, brief_attr in attrs:
+        elem = getattr(delta, attr + 's')
+        if not elem:
+            continue
 
-            if brief:
-                output.append(f'{value}{brief_name}')
-            else:
-                output.append(format(plural(value), name))
+        if attr == 'day':
+            weeks = delta.weeks
+            if weeks:
+                elem -= weeks * 7
+                if not brief:
+                    output.append(format(plural(weeks), 'week'))
+                else:
+                    output.append(f'{weeks}w')
+
+        if elem <= 0:
+            continue
+
+        if brief:
+            output.append(f'{elem}{brief_attr}')
+        else:
+            output.append(format(plural(elem), attr))
 
     if accuracy is not None:
         output = output[:accuracy]
