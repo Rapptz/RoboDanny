2e05e2f448d9a3d8be59c420fb0647ee925fb945
Rapptz <rapptz@gmail.com>
2019-05-19 22:11:54 -0400

    Special case ?source help to show the HelpCommand class.

--- cogs/meta.py
+++ cogs/meta.py
@@ -325,19 +325,27 @@ class Meta(commands.Cog):
         if command is None:
             return await ctx.send(source_url)
 
-        obj = self.bot.get_command(command.replace('.', ' '))
-        if obj is None:
-            return await ctx.send('Could not find command.')
-
-        # since we found the command we're looking for, presumably anyway, let's
-        # try to access the code itself
-        src = obj.callback.__code__
-        lines, firstlineno = inspect.getsourcelines(src)
-        if not obj.callback.__module__.startswith('discord'):
-            # not a built-in command
-            location = os.path.relpath(src.co_filename).replace('\\', '/')
+        if command == 'help':
+            src = type(self.bot.help_command)
+            module = src.__module__
+            filename = inspect.getsourcefile(src)
         else:
-            location = obj.callback.__module__.replace('.', '/') + '.py'
+            obj = self.bot.get_command(command.replace('.', ' '))
+            if obj is None:
+                return await ctx.send('Could not find command.')
+
+            # since we found the command we're looking for, presumably anyway, let's
+            # try to access the code itself
+            src = obj.callback.__code__
+            module = obj.callback.__module__
+            filename = src.co_filename
+
+        lines, firstlineno = inspect.getsourcelines(src)
+        if not module.startswith('discord'):
+            # not a built-in command
+            location = os.path.relpath(filename).replace('\\', '/')
+        else:
+            location = module.replace('.', '/') + '.py'
             source_url = 'https://github.com/Rapptz/discord.py'
 
         final_url = f'<{source_url}/blob/rewrite/{location}#L{firstlineno}-L{firstlineno + len(lines) - 1}>'
