dbcf30268eae1a9a77e14a8eacb4ee55635af839
Rapptz <rapptz@gmail.com>
2019-09-16 22:31:49 -0400

    Require 5 extraneous triggers to auto-block instead of 2.

--- bot.py
+++ bot.py
@@ -178,7 +178,7 @@ class RoboDanny(commands.AutoShardedBot):
         author_id = message.author.id
         if retry_after and author_id != self.owner_id:
             self._auto_spam_count[author_id] += 1
-            if self._auto_spam_count[author_id] >= 2:
+            if self._auto_spam_count[author_id] >= 5:
                 await self.add_to_blacklist(author_id)
                 del self._auto_spam_count[author_id]
                 await self.log_spammer(ctx, message, retry_after, autoblock=True)
