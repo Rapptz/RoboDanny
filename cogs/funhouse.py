ffa6657d9618131ef65c59488b9f3b9ff85e16c7
Rapptz <rapptz@gmail.com>
2019-11-08 17:51:54 -0500

    Add random dog command

--- cogs/funhouse.py
+++ cogs/funhouse.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 from .utils import checks
 import discord
+import io
 
 GUILD_ID = 81883016288276480
 VOICE_ROOM_ID = 633466718035116052
@@ -37,6 +38,26 @@ class Funhouse(commands.Cog):
                 return await ctx.send('No cat found :(')
             js = await resp.json()
             await ctx.send(embed=discord.Embed(title='Random Cat').set_image(url=js['file']))
+
+    @commands.command(hidden=True)
+    async def dog(self, ctx):
+        """Gives you a random dog."""
+        async with ctx.session.get('https://random.dog/woof') as resp:
+            if resp.status != 200:
+                return await ctx.send('No dog found :(')
+
+            filename = await resp.text()
+            url = f'https://random.dog/{filename}'
+            if filename.endswith(('.mp4', '.webm')):
+                async with ctx.typing():
+                    async with ctx.session.get(url) as other:
+                        if other.status != 200:
+                            return await ctx.send('Could not download dog video :(')
+
+                        fp = io.BytesIO(await other.read())
+                        await ctx.send(file=discord.File(fp, filename=filename))
+            else:
+                await ctx.send(embed=discord.Embed(title='Random Dog').set_image(url=url))
 
 def setup(bot):
     bot.add_cog(Funhouse(bot))
