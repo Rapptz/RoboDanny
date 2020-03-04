896b526582b188eb15ae6ef965f9a237ec3d326a
Rapptz <rapptz@gmail.com>
2020-03-04 08:58:27 -0500

    Add translate command for the funhouse
    Technically can be used by anyone though

--- cogs/funhouse.py
+++ cogs/funhouse.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 from .utils import checks
 import discord
+import googletrans
 import io
 
 GUILD_ID = 81883016288276480
@@ -10,6 +11,7 @@ GENERAL_VOICE_ID = 81883016309248000
 class Funhouse(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.trans = googletrans.Translator()
 
     def is_outside_voice(self, state):
         return state.channel is None or state.channel.id != GENERAL_VOICE_ID
@@ -62,6 +64,24 @@ class Funhouse(commands.Cog):
                         await ctx.send(file=discord.File(fp, filename=filename))
             else:
                 await ctx.send(embed=discord.Embed(title='Random Dog').set_image(url=url))
+
+    @commands.command(hidden=True)
+    async def translate(self, ctx, *, message: commands.clean_content):
+        """Translates a message to English using Google translate."""
+
+        loop = self.bot.loop
+
+        try:
+            ret = await loop.run_in_executor(None, self.trans.translate, message)
+        except Exception as e:
+            return await ctx.send(f'An error occurred: {e.__class__.__name__}: {e}')
+
+        embed = discord.Embed(title='Translated', colour=0x4284F3)
+        src = googletrans.LANGUAGES.get(ret.src, '(auto-detected)').title()
+        dest = googletrans.LANGUAGES.get(ret.dest, 'Unknown').title()
+        embed.add_field(name=f'From {src}', value=ret.origin, inline=False)
+        embed.add_field(name=f'To {dest}', value=ret.text, inline=False)
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Funhouse(bot))
