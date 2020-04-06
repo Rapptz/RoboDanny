371cdd6d2babc7f0963ad723a1a702acb2d05294
Rapptz <rapptz@gmail.com>
2020-04-06 19:17:21 -0400

    Bump heartbeat timeout to 2.5 minutes instead of 1 minute.

--- bot.py
+++ bot.py
@@ -54,7 +54,8 @@ def _prefix_callable(bot, msg):
 class RoboDanny(commands.AutoShardedBot):
     def __init__(self):
         super().__init__(command_prefix=_prefix_callable, description=description,
-                         pm_help=None, help_attrs=dict(hidden=True), fetch_offline_members=False)
+                         pm_help=None, help_attrs=dict(hidden=True),
+                         fetch_offline_members=False, heartbeat_timeout=150.0)
 
         self.client_id = config.client_id
         self.carbon_key = config.carbon_key
