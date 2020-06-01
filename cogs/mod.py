608374feba8d01fcf85f7067f73795d4b1522ee6
Rapptz <rapptz@gmail.com>
2020-06-01 06:34:42 -0400

    Don't fetch entire ban list if unbanning by ID.

--- cogs/mod.py
+++ cogs/mod.py
@@ -121,15 +121,18 @@ class MemberID(commands.Converter):
 
 class BannedMember(commands.Converter):
     async def convert(self, ctx, argument):
-        ban_list = await ctx.guild.bans()
-        try:
+        if argument.isdigit():
             member_id = int(argument, base=10)
-            entity = discord.utils.find(lambda u: u.user.id == member_id, ban_list)
-        except ValueError:
-            entity = discord.utils.find(lambda u: str(u.user) == argument, ban_list)
+            try:
+                return await ctx.guild.fetch_ban(discord.Object(id=member_id))
+            except discord.NotFound:
+                raise commands.BadArgument('This member has not been banned before.') from None
+
+        ban_list = await ctx.guild.bans()
+        entity = discord.utils.find(lambda u: str(u.user) == argument, ban_list)
 
         if entity is None:
-            raise commands.BadArgument("Not a valid previously-banned member.")
+            raise commands.BadArgument('This member has not been banned before.')
         return entity
 
 class ActionReason(commands.Converter):
