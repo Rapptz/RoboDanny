5def5d16f52934dc90de04e3abe40809a402f18f
Rapptz <rapptz@gmail.com>
2022-04-24 11:17:38 -0400

    Re-format api cog

--- cogs/api.py
+++ cogs/api.py
@@ -460,7 +460,14 @@ class API(commands.Cog):
 
         try:
             for channel in channels:
-                await channel.set_permissions(member, send_messages=False, add_reactions=False, create_public_threads=False, send_messages_in_threads=False, reason=reason)
+                await channel.set_permissions(
+                    member,
+                    send_messages=False,
+                    add_reactions=False,
+                    create_public_threads=False,
+                    send_messages_in_threads=False,
+                    reason=reason,
+                )
         except:
             await ctx.send('\N{THUMBS DOWN SIGN}')
         else:
@@ -483,7 +490,14 @@ class API(commands.Cog):
 
         try:
             for channel in channels:
-                await channel.set_permissions(member, send_messages=None, add_reactions=None, create_public_threads=None, send_messages_in_threads=None, reason=reason)
+                await channel.set_permissions(
+                    member,
+                    send_messages=None,
+                    add_reactions=None,
+                    create_public_threads=None,
+                    send_messages_in_threads=None,
+                    reason=reason,
+                )
         except:
             await ctx.send('\N{THUMBS DOWN SIGN}')
         else:
@@ -528,7 +542,14 @@ class API(commands.Cog):
 
         try:
             for channel in channels:
-                await channel.set_permissions(member, send_messages=False, add_reactions=False, create_public_threads=False, send_messages_in_threads=False, reason=reason)
+                await channel.set_permissions(
+                    member,
+                    send_messages=False,
+                    add_reactions=False,
+                    create_public_threads=False,
+                    send_messages_in_threads=False,
+                    reason=reason,
+                )
         except:
             await ctx.send('\N{THUMBS DOWN SIGN}')
         else:
@@ -569,7 +590,14 @@ class API(commands.Cog):
 
         for ch in self.get_block_channels(guild, channel):
             try:
-                await ch.set_permissions(to_unblock, send_messages=None, add_reactions=None, create_public_threads=None, send_messages_in_threads=None, reason=reason)
+                await ch.set_permissions(
+                    to_unblock,
+                    send_messages=None,
+                    add_reactions=None,
+                    create_public_threads=None,
+                    send_messages_in_threads=None,
+                    reason=reason,
+                )
             except:
                 pass
 
