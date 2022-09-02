c3d80bf103d8fabab442274216286281b095ba78
Rapptz <rapptz@gmail.com>
2022-09-02 09:53:45 -0400

    Fix automod upgrade query to explicitly change nulls
    I have no idea why the original query does not work in production.

--- migrations/V2__automod_changes.sql
+++ migrations/V2__automod_changes.sql
@@ -9,13 +9,10 @@ ALTER TABLE guild_mod_config ADD COLUMN broadcast_webhook_url TEXT;
 
 -- Previous versions of raid_mod = 2 implied raid_mode = 1
 -- Due to this now being interpreted as bit flags this will need to be 3 (1 | 2)
--- This also removes the NULL flag case and defaults it to zero straight up
-UPDATE guild_mod_config
-    SET
-        automod_flags = CASE COALESCE(automod_flags, -1)
-                        WHEN 2 THEN 3
-                        WHEN -1 THEN 0
-                        END;
+UPDATE guild_mod_config SET automod_flags = 3 WHERE automod_flags = 2;
+
+-- Remove all nulls and set them to zero
+UPDATE guild_mod_config SET automod_flags = 0 WHERE automod_flags IS NULL;
 
 -- Change the flags to be not null now that there are no null values
 ALTER TABLE guild_mod_config ALTER COLUMN automod_flags SET NOT NULL;
