c96b6cea1f686b6499f8514b770b222ab55c4f6a
Rapptz <rapptz@gmail.com>
2023-05-04 04:34:00 -0400

    Add U+FE0F to logging emoji

--- cogs/stats.py
+++ cogs/stats.py
@@ -699,7 +699,7 @@ class Stats(commands.Cog):
         self._logging_queue.put_nowait(record)
 
     async def send_log_record(self, record: logging.LogRecord) -> None:
-        attributes = {'INFO': '\N{INFORMATION SOURCE}', 'WARNING': '\N{WARNING SIGN}'}
+        attributes = {'INFO': '\N{INFORMATION SOURCE}\ufe0f', 'WARNING': '\N{WARNING SIGN}\ufe0f'}
 
         emoji = attributes.get(record.levelname, '\N{CROSS MARK}')
         dt = datetime.datetime.utcfromtimestamp(record.created)
