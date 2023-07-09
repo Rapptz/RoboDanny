f859a326d74e919b1b3042b0114a258cd6a531f4
Rapptz <rapptz@gmail.com>
2023-07-09 15:59:23 -0400

    Remove repr call from ?choosebestof

--- cogs/rng.py
+++ cogs/rng.py
@@ -228,7 +228,7 @@ class RNG(commands.Cog):
         fourth_winner, results = self._bestof_choices(second_winner, third_winner, 5)
         formatted_results = ', '.join('Win' if r == fourth_winner else 'Loss' for r in results)
         if fourth_winner == second_winner:
-            to_send.append(f'4. {second_winner} vs {third_winner}: **{fourth_winner!r} won the championship ({formatted_results})!**')
+            to_send.append(f'4. {second_winner} vs {third_winner}: **{fourth_winner} won the championship ({formatted_results})!**')
             return to_send
         else:
             to_send.append(f'4. {second_winner} vs {third_winner}: {fourth_winner} wins! ({formatted_results})')
