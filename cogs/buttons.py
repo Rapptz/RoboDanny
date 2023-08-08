eb8e14d78e4a1ad8ed0eea86ee30b2fbbd0c760e
Rapptz <rapptz@gmail.com>
2023-08-08 13:49:33 -0400

    Remove brotli encoding from ?define

--- cogs/buttons.py
+++ cogs/buttons.py
@@ -274,7 +274,7 @@ async def parse_free_dictionary_for_word(session: ClientSession, *, word: str) -
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
         'Accept-Language': 'en-US,en;q=0.5',
-        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Encoding': 'gzip, deflate',
         'Upgrade-Insecure-Requests': '1',
         'Sec-Fetch-Dest': 'document',
         'Sec-Fetch-Mode': 'navigate',
